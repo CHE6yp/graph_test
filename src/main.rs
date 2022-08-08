fa5aa6a239dc0355210b8b106c0760b2907633e3
CHE6yp <che6yp@gmail.com>
2022-08-08 20:24:36 +0300

    Barebones, not working

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,26 @@ fn round(x: f32, precision: u32) -> f32 {
 }
 fn main() {
     // x1, x2, x3 are input nodes of the computational graph:
-    let x1 = create_input("x1");
-    let x2 = create_input("x2");
-    let x3 = create_input("x3");
+    // let x1 = create_input("x1");
+    // let x2 = create_input("x2");
+    // let x3 = create_input("x3");
+    let x1 = create_input(1f32);
+    let x2 = create_input(2f32);
+    let x3 = create_input(3f32);
     // graph variable is the output node of the graph:
+    // let graph = add(
+    //     x1.clone(),
+    //     mul(
+    //         x2.clone(),
+    //         sin(add(x2.clone(), pow_f32(x3.clone(), Node::Variable(3f32)))),
+    //     ),
+    // );
     let graph = add(
         x1.clone(),
-        mul(x2.clone(), sin(add(x2.clone(), pow_f32(x3.clone(), 3f32)))),
+        mul(
+            x2.clone(),
+            sin(add(x2.clone(), pow_f32(x3.clone(), Node::Variable(3f32)))),
+        ),
     );
     x1.set(1f32);
     x2.set(2f32);
@@ -27,4 +40,70 @@ fn main() {
     result = round(result, 5);
     println!("Graph output = {}", result);
     assert_eq!(round(result, 5), -0.56656);
+}
+
+enum Node {
+    Computable(ComputationNode),
+    Variable(f32),
+}
+
+impl Node {
+    fn compute(&self) -> f32 {
+        match self {
+            Node::Computable(n) => n.compute(),
+            Node::Variable(v) => *v,
+        }
+    }
+
+    fn set(&self, x: f32) {
+        self = &Node::Variable(x);
+    }
+}
+
+struct ComputationNode {
+    input: Vec<Node>,
+    computation: Box<dyn Fn(Vec<f32>) -> f32>,
+}
+
+impl ComputationNode {
+    fn compute(&self) -> f32 {
+        let v = self.input.iter().map(|x| x.compute()).collect();
+        (self.computation)(v)
+    }
+}
+
+fn create_input(x: f32) -> Node {
+    Node::Variable(x)
+}
+
+fn add(first: Node, second: Node) -> Node {
+    let v = vec![first, second];
+    Node::Computable(ComputationNode {
+        input: v,
+        computation: Box::new(|a: Vec<f32>| a[0] + a[1]),
+    })
+}
+
+fn mul(first: Node, second: Node) -> Node {
+    let v = vec![first, second];
+    Node::Computable(ComputationNode {
+        input: v,
+        computation: Box::new(|a: Vec<f32>| a[0] * a[1]),
+    })
+}
+
+fn sin(x: Node) -> Node {
+    let v = vec![x];
+    Node::Computable(ComputationNode {
+        input: v,
+        computation: Box::new(|a: Vec<f32>| a[0].sin()),
+    })
+}
+
+fn pow_f32(first: Node, second: Node) -> Node {
+    let v = vec![first, second];
+    Node::Computable(ComputationNode {
+        input: v,
+        computation: Box::new(|a: Vec<f32>| a[0].powf(a[1])),
+    })
 }
